acd6adfe9323d0529e6d0d4553d30f6205162b18
Justin Farnsworth <farnswj1@tcnj.edu>
2024-07-18 15:33:04 -0400

    fix typo in comment

--- app/src/handlers/mod.rs
+++ app/src/handlers/mod.rs
@@ -9,7 +9,7 @@ pub async fn plain() -> &'static str {
     "This is plain text!"
 }
 
-// Tuples can be return to specify status codes. The default code is 200 (OK)
+// Tuples can be returned to specify status codes. The default code is 200 (OK)
 pub async fn json() -> (StatusCode, Json<User>) {
     let data = User { id: 12, name: String::from("John Smith")};
     (StatusCode::OK, Json(data))
